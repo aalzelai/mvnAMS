bfa5878631181886470df92400f6ef77cdab71fa
aalzelai <andoni.alcelay@alumni.mondragon.edu>
2016-12-01 11:17:47 +0100

    Correct travis.yml and add bat and sh

---
+++ tomcatStart.bat
@@ -0,0 +1,5 @@
+SET CATALINA_HOME = %JRE_HOME%
+cd tomcat8
+cd bin
+start http://192.168.22.3:8080/mvnAMS
+startup.bat
---
+++ tomcatStart.sh
@@ -0,0 +1,7 @@
+ls -aFl
+cd tomcat8
+ls -aFl
+cd bin
+ls -aFl
+start http://localhost:8080/mvnAMS
+sh startup.sh
