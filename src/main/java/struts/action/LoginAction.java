52f0514bf2aa5be8f58bcce7ceb5b4fa78f44354
aalzelai <andoni.alcelay@alumni.mondragon.edu>
2016-12-14 22:40:41 +0100

    Passenger index window. It has a table with current flights

--- src/main/java/struts/action/LoginAction.java
+++ src/main/java/struts/action/LoginAction.java
@@ -1,66 +1,67 @@
 package struts.action;
+import java.util.List;
+
 import com.opensymphony.xwork2.ActionSupport;
 
-import domain.dao.UserFacade;
+import domain.dao.DaoUserHibernate;
 import domain.model.User;
 
-/**
- * Class of the Structs2 of the project.
- * @author PBL5
- *
- */
 public class LoginAction extends ActionSupport {
     /**
-     * The serial version UID.
-     */
-    private static final long serialVersionUID = 1L;
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private User user;
+	private List<User> userList;
+	private DaoUserHibernate userDao;
+    
+    public LoginAction() {
+    	userDao = new DaoUserHibernate();
+	}
 
-    /**
-     * The username.
-     */
-    private String username;
-
-    /**
-     * The password.
-     */
-    private String password;
-
-
-    /**
-     * The function to execute.
-     * @return how goes the execution.
-     */
+ 
     public String execute() {
+ 
+		/*System.out.println("Username: "+user.getUsername());
+		System.out.println("Password: "+user.getPassword());
+		
+		if(user.getUsername()!=null && user.getPassword()!=null){
+			
+		}*/
+    	
+    	userDao = new DaoUserHibernate();
+    	
+    	System.out.println("LLEGA 1");
+    	
+		userList = userDao.loadUser();
+		
+    	System.out.println("LLEGA 2");
+    	
+    	System.out.println("Hay " + userList.size() + " usuarios");
 
-        User user = null;
-        System.out.println("Username: " + username);
-        System.out.println("Password: " + password);
-
-        UserFacade uf = new UserFacade();
-        if (username != null && password != null) {
-            user = uf.loadUser(username, password);
-        }
-
-        if (user != null) {
-            return "success1";
-        } else {
-            return "error";
-        }
-
-        /*
-        System.out.println("user: "+this.username);
+		return "success2";
+		
+		/*if(user != null){
+			return "success1";
+		}else{
+			return "error";
+		}*/
+    	
+    	
+    	/*
+    	System.out.println("user: "+this.username);
         if (this.username.equals("admin1")
                 && this.password.equals("admin")) {
             return "success1";
         } else if(this.username.equals("admin2")
                 && this.password.equals("admin")) {
-            return "success2";
+        	return "success2";
         }else if(this.username.equals("admin3")
                 && this.password.equals("admin")) {
-            return "success3";
+        	return "success3";
         }else if(this.username.equals("admin4")
                 && this.password.equals("admin")) {
-            return "success4";
+        	return "success4";
         }
         else {
             addActionError(getText("error.login"));
@@ -68,37 +69,27 @@ public class LoginAction extends ActionSupport {
         }
         */
     }
+ 
+    public User getUser() {
+		return user;
+	}
 
-    /**
-     * Getter of the username.
-     * @return the username
-     */
-    public String getUsername() {
-        return username;
-    }
+	public void setUser(User user) {
+		this.user = user;
+	}
 
-    /**
-     * Setter of the username.
-     * @param username is the username
-     */
-    public void setUsername(final String username) {
-        this.username = username;
-    }
 
-    /**
-     * Getter of the password.
-     * @return the password
-     */
-    public String getPassword() {
-        return password;
-    }
+	public List<User> getUserList() {
+		return userList;
+	}
 
-    /**
-     * Setter of the password.
-     * @param password is the password
-     */
-    public void setPassword(final String password) {
-        this.password = password;
-    }
+
+	public void setUserList(List<User> userList) {
+		this.userList = userList;
+	}
+	
+	
+    
+    
 }
 
