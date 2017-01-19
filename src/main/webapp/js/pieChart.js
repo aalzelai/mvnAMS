c3a3a0e7429b6eda46451d79b801d0ca680bb5cf
aalzelai <andoni.alcelay@alumni.mondragon.edu>
2017-01-19 11:09:01 +0100

    Now we have an interactive dynamic and very beautiful donut chart

--- src/main/webapp/js/pieChart.js
+++ src/main/webapp/js/pieChart.js
@@ -2,7 +2,9 @@
  * 
  */
 
-var total = 0;
+(function(d3) {
+        'use strict';
+
         var width = 360;
         var height = 360;
         var radius = Math.min(width, height) / 2;
@@ -14,8 +16,8 @@ var total = 0;
 
         var svg = d3.select('#chart')
           .append('svg')
-          .attr('width', 600)
-          .attr('height', 600)
+          .attr('width', width)
+          .attr('height', height)
           .append('g')
           .attr('transform', 'translate(' + (width / 2) +
             ',' + (height / 2) + ')');
@@ -41,10 +43,10 @@ var total = 0;
         tooltip.append('div')
           .attr('class', 'percent');
 
-        d3.csv('csv/data.csv', function(error, dataset) {
+        d3.csv('csv/pieChart.csv', function(error, dataset) {
           dataset.forEach(function(d) {
             d.count = +d.count;
-            d.enabled = true;                                    
+            d.enabled = true;                                        
           });
 
           var path = svg.selectAll('path')
@@ -54,30 +56,24 @@ var total = 0;
             .attr('d', arc)
             .attr('fill', function(d, i) {
               return color(d.data.label);
-            })                                                    
-            .each(function(d) { this._current = d; });      
+            })                                                       
+            .each(function(d) { this._current = d; });                
 
           path.on('mouseover', function(d) {
             var total = d3.sum(dataset.map(function(d) {
-              return (d.enabled) ? d.count : 0;                  
+              return (d.enabled) ? d.count : 0;                       
             }));
             var percent = Math.round(1000 * d.data.count / total) / 10;
             tooltip.select('.label').html(d.data.label);
             tooltip.select('.count').html(d.data.count);
             tooltip.select('.percent').html(percent + '%');
             tooltip.style('display', 'block');
+            tooltip.style('opacity', '1');
           });
 
           path.on('mouseout', function() {
             tooltip.style('display', 'none');
           });
-
-          /* OPTIONAL
-          path.on('mousemove', function(d) {
-            tooltip.style('top', (d3.event.layerY + 10) + 'px')
-              .style('left', (d3.event.layerX + 10) + 'px');
-          });
-          */
 
           var legend = svg.selectAll('.legend')
             .data(color.domain())
@@ -87,49 +83,48 @@ var total = 0;
             .attr('transform', function(d, i) {
               var height = legendRectSize + legendSpacing;
               var offset =  height * color.domain().length / 2;
-              var horz = 200;
-              var vert = 10 * dataset.length+(i * height - offset);
+              var horz = -2 * legendRectSize;
+              var vert = i * height - offset;
               return 'translate(' + horz + ',' + vert + ')';
             });
-
 
           legend.append('rect')
             .attr('width', legendRectSize)
             .attr('height', legendRectSize)
             .style('fill', color)
-            .style('stroke', color)                         
-            .on('click', function(label) {                           
-              var rect = d3.select(this);                           
-              var enabled = true;                                  
-              var totalEnabled = d3.sum(dataset.map(function(d) {    
-                return (d.enabled) ? 1 : 0;                           
-              }));                                                  
+            .style('stroke', color)                                   
+            .on('click', function(label) {                            
+              var rect = d3.select(this);                             
+              var enabled = true;                                     
+              var totalEnabled = d3.sum(dataset.map(function(d) {     
+                return (d.enabled) ? 1 : 0;                         
+              }));                                                   
 
-              if (rect.attr('class') === 'disabled') {            
+              if (rect.attr('class') === 'disabled') {                
                 rect.attr('class', '');                            
-              } else {                                           
-                if (totalEnabled < 2) return;                     
-                rect.attr('class', 'disabled');                      
-                enabled = false;                                   
-              }                                                  
+              } else {                                                
+                if (totalEnabled < 2) return;                         
+                rect.attr('class', 'disabled');                     
+                enabled = false;                                     
+              }                                                       
 
-              pie.value(function(d) {                               
-                if (d.label === label) d.enabled = enabled;       
-                return (d.enabled) ? d.count : 0;                 
-              });                                                
+              pie.value(function(d) {                              
+                if (d.label === label) d.enabled = enabled;          
+                return (d.enabled) ? d.count : 0;                     
+              });                                                   
 
-              path = path.data(pie(dataset));                        
+              path = path.data(pie(dataset));                         
 
               path.transition()                                       
-                .duration(750)                                        
-                .attrTween('d', function(d) {                      
-                  var interpolate = d3.interpolate(this._current, d);
-                  this._current = interpolate(0);                     
+                .duration(750)                                       
+                .attrTween('d', function(d) {                        
+                  var interpolate = d3.interpolate(this._current, d); 
+                  this._current = interpolate(0);                   
                   return function(t) {                                
-                    return arc(interpolate(t));                      
-                  };                                               
-                });                                                   
-            });                                                       
+                    return arc(interpolate(t));                   
+                  };                                                 
+                });                                                  
+            });                                                      
 
           legend.append('text')
             .attr('x', legendRectSize + legendSpacing)
@@ -137,104 +132,5 @@ var total = 0;
             .text(function(d) { return d; });
 
         });
-        
-        
-        function update(data){
-        	d3.csv('csv/data.csv', function(error, dataset) {
-                dataset.forEach(function(d) {
-                  d.count = +d.count;
-                  d.enabled = true;                                    
-                });
-                var path = svg.selectAll('path')
-                  .data(pie(dataset))
-                  .enter()
-                  .append('path')
-                  .attr('d', arc)
-                  .attr('fill', function(d, i) {
-                    return color(d.data.label);
-                  })                                                    
-                  .each(function(d) { this._current = d; });      
 
-                path.on('mouseover', function(d) {
-                  var total = d3.sum(dataset.map(function(d) {
-                    return (d.enabled) ? d.count : 0;                  
-                  }));
-                  var percent = Math.round(1000 * d.data.count / total) / 10;
-                  tooltip.select('.label').html(d.data.label);
-                  tooltip.select('.count').html(d.data.count);
-                  tooltip.select('.percent').html(percent + '%');
-                  tooltip.style('display', 'block');
-                });
-
-                path.on('mouseout', function() {
-                  tooltip.style('display', 'none');
-                });
-
-                /* OPTIONAL
-                path.on('mousemove', function(d) {
-                  tooltip.style('top', (d3.event.layerY + 10) + 'px')
-                    .style('left', (d3.event.layerX + 10) + 'px');
-                });
-                */
-
-                var legend = svg.selectAll('.legend')
-                  .data(color.domain())
-                  .enter()
-                  .append('g')
-                  .attr('class', 'legend')
-                  .attr('transform', function(d, i) {
-                    var height = legendRectSize + legendSpacing;
-                    var offset =  height * color.domain().length / 2;
-                    var horz = 200;
-                    var vert = 10 * dataset.length+(i * height - offset);
-                    return 'translate(' + horz + ',' + vert + ')';
-                  });
-
-
-                legend.append('rect')
-                  .attr('width', legendRectSize)
-                  .attr('height', legendRectSize)
-                  .style('fill', color)
-                  .style('stroke', color)                         
-                  .on('click', function(label) {                           
-                    var rect = d3.select(this);                           
-                    var enabled = true;                                  
-                    var totalEnabled = d3.sum(dataset.map(function(d) {    
-                      return (d.enabled) ? 1 : 0;                           
-                    }));                                                  
-
-                    if (rect.attr('class') === 'disabled') {            
-                      rect.attr('class', '');                            
-                    } else {                                           
-                      if (totalEnabled < 2) return;                     
-                      rect.attr('class', 'disabled');                      
-                      enabled = false;                                   
-                    }                                                  
-
-                    pie.value(function(d) {                               
-                      if (d.label === label) d.enabled = enabled;       
-                      return (d.enabled) ? d.count : 0;                 
-                    });                                                
-
-                    path = path.data(pie(dataset));                        
-
-                    path.transition()                                       
-                      .duration(750)                                        
-                      .attrTween('d', function(d) {                      
-                        var interpolate = d3.interpolate(this._current, d);
-                        this._current = interpolate(0);                     
-                        return function(t) {                                
-                          return arc(interpolate(t));                      
-                        };                                               
-                      });                                                   
-                  });                                                       
-
-                legend.append('text')
-                  .attr('x', legendRectSize + legendSpacing)
-                  .attr('y', legendRectSize - legendSpacing)
-                  .text(function(d) { return d; });
-
-              });
-        }
-        update();
-        setTimeout( update, 1000);
+      })(window.d3);
--- src/main/webapp/pages/AirlineGraphics.jsp
+++ src/main/webapp/pages/AirlineGraphics.jsp
@@ -1,4 +1,4 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
+s<%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 <%@ taglib prefix="s" uri="/struts-tags"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -9,63 +9,21 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="css/bootstrap.min.css">
-<link rel="stylesheet" href="css/index.css">
+<link rel="stylesheet" href="css/pieChart.css">
 <script src="js/jquery-3.1.1.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 
 	<script type="text/javascript" src= "js/d3/d3.js"></script>
 	<%--<script src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script> --%>
-	
 </head>
-<style>
- #chart {
-        height: 360px;
-        margin: 0 auto;                                         
-        position: relative;
-        width: 360px;
-        margin-top:10px;
-        margin-bottom:50px;
-      }
-      .tooltip {
-        background: #eee;
-        box-shadow: 0 0 5px #999999;
-        color: #333;
-        display: none;
-        font-size: 12px;
-        left: 130px;
-        padding: 10px;
-        position: absolute;
-        text-align: center;
-        top: 95px;
-        width: 80px;
-        z-index: 10;
-      }
-      .legend {
-        font-size: 12px;
-      }
-      rect {
-        cursor: pointer;                                          
-        stroke-width: 2;
-      }
-      rect.disabled {                                               
-        fill: transparent !important;                          
-      }                                                         
-      h1 {                                                   
-        font-size: 14px;                                        
-        text-align: center;                                      
-      } 
 
-</style>
 <body>
+
 <div id="chart">
+
 		<h2>Percentage of flight timing</h2>
 	</div>
 	<script src="js/pieChart.js"></script>
-<script type="text/javascript">
-$(document).ready(function() {
-	
-});
-</script>
 
 </body>
 </html>
